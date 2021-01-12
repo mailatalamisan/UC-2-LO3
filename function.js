d95089876727f189fbc8171acf22b8742b540210
mailatalamisan <76142191+mailatalamisan@users.noreply.github.com>
2021-01-12 18:49:54 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,11 @@
+function getAverage (a,b,c,d,e,f){
+
+var average = (a + b + c + d +e + f) / 6;
+console.log(average);
+return average;
+
+}
+
+
+var myResult = getAverage(7,8,9,10,11,12);
+console.log("the average is " + myResult);
